bee189ee11f311a380cc122bf5cec6b1fe839770
Ashley <ashley@Zipcoders-MBP-3.mac.lan>
2022-06-23 20:48:34 -0400

    new code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner; 
+
 public class Main {
 
     public static void main(String[] args ){
+	Scanner abObj = new Scanner(System.in);  // Create a Scanner object
+	
+	System.out.println("Please enter your name");
 
+	String name = abObj.nextLine();  // Read user input
+	
+	if(name.equals("Alice") || name.equals("Bob")) {
+		System.out.println("Hi " + name + " nice to meet you!");
+	}
     }
 }
